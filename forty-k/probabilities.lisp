8d7057d593309b82bc6991b3535c6b8be5257804
BigDawn <ddale507@hotmail.co.uk>
2024-06-25 20:01:21 +0100

    Add d6 probability func and start fortyk probs

---
+++ forty-k/probabilities.lisp
@@ -0,0 +1,14 @@
+
+;;; Return probability as a decimal
+
+(defun hit-probability (shooter keywoid)
+  ;; TODO getShooterHitSkillByKeyword
+  ;; TODO CalculateDecimalProbabilityOnD6
+  )
+
+(defun wound-probability (shooter enemy)
+  )
+
+(defun no-save-probability (shooter enemy)
+  )
+
