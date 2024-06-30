ba5e8ed17ea2b6f01a840927ba6c36e0b4474f5f
BigDawn <ddale507@hotmail.co.uk>
2024-06-30 20:00:10 +0100

    Add start of hit prob. Stop because I forgot to load shooter stats.

--- forty-k/probabilities.lisp
+++ forty-k/probabilities.lisp
@@ -1,9 +1,10 @@
 
 ;;; Return probability as a decimal
 
-(defun hit-probability (shooter keywoid)
+(defun hit-probability (shooter)
   ;; TODO getShooterHitSkillByKeyword
   ;; TODO CalculateDecimalProbabilityOnD6
+  (d6-probability (stat-find "Ballistic-Skill" shooter))
   )
 
 (defun wound-probability (shooter enemy)
--- loading.lisp
+++ loading.lisp
@@ -5,7 +5,7 @@
   "Verify game-name has a folder. Use folder marked by game name."
   )
 
-(defparameter *units-csv* (cl-csv:read-csv #P"units.csv"))
+(defparameter *units-csv* (cl-csv:read-csv #P"forty-k/units.csv"))
 (defparameter *units* (make-hash-table :test 'EQUAL))
 (hash-csv-vectors (pop *units-csv*) *units-csv* "Model" *units*)
 
