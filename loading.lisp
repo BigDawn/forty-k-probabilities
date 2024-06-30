dcc1d3ee6d8105e9d38540910b7d3ecdf4dca9d6
BigDawn <ddale507@hotmail.co.uk>
2024-06-30 19:10:52 +0100

    Add skeleton of app per game build system.

--- loading.lisp
+++ loading.lisp
@@ -1,3 +1,10 @@
+;;;; With an aim to expanding to other rulesets, I should look to 
+;;;; creating a build system that spits out apps per game
+
+(defun load-which-game (game-name)
+  "Verify game-name has a folder. Use folder marked by game name."
+  )
+
 (defparameter *units-csv* (cl-csv:read-csv #P"units.csv"))
 (defparameter *units* (make-hash-table :test 'EQUAL))
 (hash-csv-vectors (pop *units-csv*) *units-csv* "Model" *units*)
